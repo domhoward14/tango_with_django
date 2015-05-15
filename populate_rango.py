dab99945dbd85cd508df3388b974367bdba940d9
eric howard <domhoward14@gmail.com>
2015-05-15 03:08:54 -0400

    Updated and completed populate test script with no more bugs

--- populate_rango.py
+++ populate_rango.py
@@ -7,8 +7,8 @@ django.setup()
 from rango.models import Category, Page
 
 def add_page (cat,title,url, views = 0):
-    p = Page.objects.get_or_create(category=cat, title = title, url= url, views = views)[0]
-    return p
+    Page.objects.get_or_create(category=cat, title = title, url= url, views = views)
+
 
 def add_cat(name):
     c = Category.objects.get_or_create(name = name)[0]
@@ -20,8 +20,38 @@ def populate():
     add_page(cat = python_cat,
              title = "Official Python Tutorial",
              url = "http://docs.python.org/2/tutorial/")
-    add_page(python_cat, "Learn Python in 10 minutes", "http//:www.korokithakis.net/tutorials/python")
 
+    add_page(python_cat,
+             "Learn Python in 10 minutes",
+             "http//:www.korokithakis.net/tutorials/python")
+
+    add_page(cat = python_cat,
+             title = "How to Think like a Computer Scientist",
+             url = "http://www.korokithakis.net/tutorials/python")
+
+    django_cat = add_cat(name = "Django")
+
+    add_page(cat = django_cat,
+             title = "Official Django Tutorial",
+             url = "https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+
+    add_page(cat = django_cat,
+             title = "Django Rocks",
+             url = "http://www.djangorocks.com/")
+
+    add_page(cat = django_cat,
+             title = "How to Tango with Django",
+             url = "http://www.tangowithdjango.com/")
+
+    frame_cat = add_cat("Other Frameworks")
+
+    add_page(frame_cat,
+             "Bottle",
+             "http://bottlepy.org/docs/dev")
+
+    add_page(frame_cat,
+             "Flask",
+             "http://flask.pocoo.org")
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category = c):
